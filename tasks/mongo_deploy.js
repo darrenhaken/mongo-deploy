8611fc9e98deec2c7b6bc32b4cc3a66bfc9258da
darren.haken <darrenhaken@gmail.com>
2013-10-17 12:15:04 +0100

    Added .idea folder to .gitignore.
    Changed the urls to darrenhaken from darren.haken in the package.json.
    Added in some initial config for the task.
    
    Resolved jshint issues with Grunt template.

--- tasks/mongo_deploy.js
+++ tasks/mongo_deploy.js
@@ -8,43 +8,44 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('mongo_deploy', 'Deploys Mongo JavaScript to a Mongo database', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    grunt.registerMultiTask('mongo_deploy', 'Deploys Mongo JavaScript to a Mongo database', function () {
+        var options = this.options();
+
+        // Merge task-specific and/or target-specific options with these defaults.
+        //grunt.verbose.writeln('Using Options: ' + JSON.stringify(options, null, 4).cyan);
+
+        //TODO DH Sort the files by date
+        //TODO DH Then enumerate through files and execute
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                    // Read file source.
+                    return grunt.file.read(filepath);
+                }).join(grunt.util.normalizelf(options.separator));
+
+            // Handle options.
+            src += options.punctuation;
+
+            // Write the destination file.
+            grunt.file.write(f.dest, src);
+
+            // Print a success message.
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
     });
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
 };
